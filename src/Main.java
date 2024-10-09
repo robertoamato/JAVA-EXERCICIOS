763950a9f5d4b9b28393f13347e65adb9296b4b2
robertoamato <robertoamatoz@gmail.com>
2024-10-09 16:59:19 -0300

    Declarações de variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //variaveis tipos
+
+       int idade = 41;
+       int salario= 4000;
+
+    // variaveis dentro do escopo local
+        var nome="roberto";
+        var idade=41;
     }
 }
