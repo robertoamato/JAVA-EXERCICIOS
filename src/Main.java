ad50ad43d8b29e6afde55526dbb0c3d22223e448
robertoamato <robertoamatoz@gmail.com>
2024-10-15 14:28:41 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,50 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+//public class Main {
+  //  public static void main(String[] args) {
         // Cria um objeto Scanner para receber entrada do usuário
-        Scanner scanner = new Scanner(System.in);
+    //    Scanner scanner = new Scanner(System.in);
 
-        // Solicita ao usuário que insira um número inteiro
-        System.out.println("Digite um número inteiro: ");
-        int exercicio = scanner.nextInt();
+
+      //  System.out.println("Digite um número inteiro: ");
+        //int exercicio = scanner.nextInt();
 
         // Verifica se o número é positivo, negativo ou zero
-        if (exercicio > 0) {
-            System.out.println(exercicio + " é um número positivo");
-        } else if (exercicio < 0) {
-            System.out.println(exercicio + " é um número negativo");
-        } else {
-            System.out.println("O número é zero");
-        }
+        //if (exercicio > 0) {
+          //  System.out.println(exercicio + " é um número positivo");
+        //} else if (exercicio < 0) {
+         //   System.out.println(exercicio + " é um número negativo");
+        //} else {
+          //  System.out.println("O número é zero");
+        //}
 
-        // Fecha o scanner
-        scanner.close();
+
+        //scanner.close();
+    //}
+
+
+    public class Main {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+
+            System.out.println("Digite a sua idade: ");
+            int idade = scanner.nextInt();
+
+            // Verifica se o número é positivo, negativo ou zero
+            if (idade > 18) {
+                System.out.println(idade + " voce é maior de idade");
+            } else if (idade < 18) {
+                System.out.println(idade + " voce é menor de idade");
+            } else {
+                System.out.println("O número é zero");
+            }
+
+            // Fecha o scanner
+            scanner.close();
+        }
     }
-}
+
 
 
