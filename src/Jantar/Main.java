225ee7fff62cfdbdfba951569305091f0415f0c2
robertoamato <robertoamatoz@gmail.com>
2024-11-20 18:18:15 -0300

    1

---
+++ src/Jantar/Main.java
@@ -0,0 +1,13 @@
+package Jantar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa pessoa1 = new Pessoa();
+
+
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.peso);
+        System.out.println(pessoa1.idade);
+     }
+}
---
+++ src/Jantar/Pessoa.java
@@ -0,0 +1,8 @@
+package Jantar;
+
+public class Pessoa {
+
+    int peso=85;
+    String nome="marcelo";
+    int idade=41;
+}
